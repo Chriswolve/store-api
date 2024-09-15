dd5709ba25219a194ecd64b91fa23f9f1b36d349
Chriswolve <christian.rojagsg@pucp.edu.pe>
2024-09-14 22:13:24 -0500

    (fix) swagger fixes

--- api/src/frameworks/services/SwaggerService.js
+++ api/src/frameworks/services/SwaggerService.js
@@ -7,14 +7,21 @@ const swaggerDocument = JSON.parse(
   readFileSync(resolve('api/src/frameworks/docs/swagger.json'), 'utf-8')
 );
 
-
+// Modificar el servidor según el entorno (local o producción)
 swaggerDocument.servers[0].url = settings.APP_URL;
-swaggerDocument.servers[0].description = settings.LOCAL?'Development':'Production';
+swaggerDocument.servers[0].description = settings.LOCAL ? 'Development' : 'Production';
 
+// Ruta para Swagger UI
+const swaggerOptions = {
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+  customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+};
+
+// Configurar Swagger UI
 export const docsUI = swaggerUi.serve;
-export const docsRoutes = swaggerUi.setup(swaggerDocument);
+export const docsRoutes = swaggerUi.setup(swaggerDocument, swaggerOptions);
 
 export default {
   docsUI,
-  docsRoutes
+  docsRoutes,
 };
